c8bdbcb3450ba9a7ec603bd52610bf18f42e6724
ju570r1u5 <albertodeprezzo@gmail.com>
2023-10-24 22:41:07 +0200

    two_sum refactoring

--- two_sum/__init__.py
+++ two_sum/__init__.py
@@ -5,9 +5,9 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         res: List[int] = []
         lns_inner: int = len(nums)
-        lns: int = lns_inner - 1
+        lns_outer: int = lns_inner - 1
 
-        for i in range(lns):
+        for i in range(lns_outer):
             for j in range(i+1, lns_inner):
                 if nums[i] + nums[j] == target:
                     res = [i, j]
@@ -15,4 +15,3 @@ class Solution:
 
         # empty return
         return res
-
